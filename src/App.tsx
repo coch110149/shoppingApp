e508bdac01ce24bdc014b9d285bf7973f10ec2c7
Clinton <clinton.edward.cochrane@gmail.com>
2023-05-25 12:54:51 -0500

    add in ability to register users and login users through firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
-import ViewMessage from './pages/ViewMessage';
+import app from  './firebaseConfig';
+import AuthPage from './pages/AuthPage';
+import { useEffect } from 'react';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -22,25 +24,26 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import firebaseConfig from './firebaseConfig';
+
 
 setupIonicReact();
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+  useEffect(() => {
+    firebaseConfig;
+  }, []);
+
+  return(
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/" exact={true}>
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home" exact={true}>
-          <Home />
-        </Route>
-        <Route path="/message/:id">
-           <ViewMessage />
-        </Route>
+        <Route path= "/auth" component={AuthPage}/>
+        <Redirect exact from="/" to="/auth"/>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
 );
+  };
 
 export default App;
